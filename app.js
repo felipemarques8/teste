412b50e32a8e8c575896538b30cd29bc3aaeef99
Felipe Marques <lipemarques8@gmail.com>
2016-11-25 19:32:17 -0300

    first commit

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
-var contas = require('./contas.json');
+'use strict';
 
-contas.sort(function(a,b){
-	return (Number(a['code']) > Number(b['code'])) ? 1 :
-		   ((Number(a['code']) < Number(b['code'])) ? -1 : 0);
-});
+const objs = require('./contas.json');
 
-console.log(contas);
+function mapToChildrens (objs) {
+  objs = objs.sort(sortByDotsCount);
+  const topCount = getCodeDotsCount(objs[0]);
+  const elders = objs.filter(filterByDotsCount(topCount));
+
+  return elders.map(mapElders);
+
+  function mapElders (elder) {
+    const descendants = getDescendants(objs, elder.code);
+    elder.children = descendants.length > 0 ? mapToChildrens(descendants) : [];
+    return elder;
+  }
+}
+function getDescendants (objs, parentCode) {
+  return objs.filter(item => item.code.startsWith(parentCode) && item.code !== parentCode);
+}
+function filterByDotsCount (equal) {
+  return item => getCodeDotsCount(item) === equal;
+}
+function sortByDotsCount (a, b) {
+  return getCodeDotsCount(a) - getCodeDotsCount(b);
+}
+function getCodeDotsCount (item) {
+  return item.code.split('.').length;
+}
+
+console.log(JSON.stringify(mapToChildrens(objs)));
